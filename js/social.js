bad78115b75e95f5a4ee3bd71e28f5fa77d1fd7c
Yonathan Pineda <yonathan.pineda@vascarsolutions.com>
2018-07-09 17:43:58 +0200

    fix index and about us styles, separate some components into parts

--- js/social.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-  $('.linkedin').children().html('<img src="./wp-content/themes/omtheme/assets/linkedin.png" />')
-  $('.twitter').children().html('<img src="./wp-content/themes/omtheme/assets/twitter.png" />')
-  $('.email').children().html('<img src="./wp-content/themes/omtheme/assets/email.png" />')
-  $('.facebook').children().html('<img src="./wp-content/themes/omtheme/assets/facebook.png" />')
-})

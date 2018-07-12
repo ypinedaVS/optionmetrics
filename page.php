29bf991524537263284eeb9e0fddc10a03d7d573
Yonathan Pineda <yonathan.pineda@vascarsolutions.com>
2018-07-12 16:42:02 +0200

    Merge succeed!

--- page.php
+++ page.php
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-<?php
-  get_header();
-
-  if( is_page( 'about-us' )) $tag = 'about-us';
-  if( is_page( 'data-products' )) $tag = 'data-products';
-  if( is_page( 'news-events' )) $tag = 'news-events';
-  if( is_page( 'contact' )) $tag = 'contact';
-  if( is_page( 'privacy-policy' )) $tag = 'privacy-policy';
-
-  $args = array(
-    'category_name' => 'home-slider',
-    'tax_query'     => array( array(
-      'taxonomy'  => 'post_tag',
-      'field'     => 'slug',
-      'terms'     => $tag
-    )));
-    $header = get_posts( $args );
-    setup_postdata( $header[0] );
-?>
-
-  <header class="masthead masthead-part d-flex align-items-center justify-content-center" style="background-image: url(<?php getImageURLFromPost(); ?>)">
-    <p class="blender-pro-bold page-hero-title px-4 text-white text-center"><?= $header[0]->post_title; ?></p>
-  </header>
-=======
 <?php get_template_part( 'partials/page', 'hero' ); ?>
->>>>>>> fixes-scss-template-parts
 
   <!-- About page -->
   <?php if( is_page( 'about-us') ) :
@@ -282,7 +256,7 @@
         <strong>After-hours support -</strong> ungent issues only<br>
         (800) 269-9192<br>
       </address>
-      
+
     </div>
   </div>
   <?php endif; ?>

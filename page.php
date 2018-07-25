0e1f0256b8bdd1e3ccce8c88be97a87f8b545966
Yonathan Pineda <yonathan.pineda@vascarsolutions.com>
2018-07-25 12:04:07 +0200

    Fixes in data-products page

--- page.php
+++ page.php
@@ -9,55 +9,13 @@
   <!-- Data Products page -->
   <?php if( is_page( 'data-products') ) : ?>
   <div class="container">
-    <div class="position-relative page-wrapper-up">
-      <div class="row d-flex align-items-center mb-5 mx-0">
-        <div class="col-md-5 col-xl-6 pt-5">
-          <img src="<?php gtdu('/assets/us_map.png'); ?>" alt="Map" class="img-fluid map us">
-          <img src="<?php gtdu('/assets/canada_map.png'); ?>" alt="Map" class="img-fluid map canada d-none">
-          <img src="<?php gtdu('/assets/europe_map.png'); ?>" alt="Map" class="img-fluid map europe d-none">
-          <img src="<?php gtdu('/assets/asia_map.png'); ?>" alt="Map" class="img-fluid map asia d-none">
-          <img src="<?php gtdu('/assets/Map_Global.png'); ?>" alt="Map" class="img-fluid map global d-none">
-        </div>
-        <div class="col-md-7 col-xl-6">
-          <nav>
-            <div class="nav nav-tabs justify-content-center" id="nav-tab" role="tablist" style="margin-bottom: 20px;">
-              <a class="nav-item nav-link active" id="nav-tab-ivydb" data-toggle="tab" href="#nav-ivydb" role="tab" aria-controls="nav-ivydb" aria-selected="true">IvyDB</a>
-              <a class="nav-item nav-link" id="nav-tab-optigraph" data-toggle="tab" href="#nav-optigraph" role="tab" aria-controls="nav-optigraph" aria-selected="false">OptiGraph</a>
-            </div>
-          </nav>
-          <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-ivydb" role="tabpanel" aria-labelledby="nav-ivydb-tab">
-              <nav>
-                <div class="nav nav-tabs justify-content-between" id="nav-tab-country" role="tablist" style="border:none!important;margin-bottom:30px;">
-                  <a class="nav-item nav-link active nav-item-country" id="nav-tab-country-us" data-toggle="tab" data-country="us" href="#nav-us" role="tab" aria-controls="nav-us" aria-selected="true">US</a>
-                  <a class="nav-item nav-link nav-item-country" id="nav-tab-country-canada" data-toggle="tab" data-country="canada" href="#nav-canada" role="tab" aria-controls="nav-canada" aria-selected="false">Canada</a>
-                  <a class="nav-item nav-link nav-item-country" id="nav-tab-country-europe" data-toggle="tab" data-country="europe" href="#nav-europe" role="tab" aria-controls="nav-europe" aria-selected="true">Europe</a>
-                  <a class="nav-item nav-link nav-item-country" id="nav-tab-country-asia" data-toggle="tab" data-country="asia" href="#nav-asia" role="tab" aria-controls="nav-asia" aria-selected="false">Asia</a>
-                  <a class="nav-item nav-link nav-item-country" id="nav-tab-country-global" data-toggle="tab" data-country="global" href="#nav-global" role="tab" aria-controls="nav-global" aria-selected="false">Global</a>
-                </div>
-              </nav>
-              <div class="tab-content" id="nav-tab-countryContent">
-                <div class="tab-pane fade show active" id="nav-us" role="tabpanel">
-                  <p class="section-text lato-light text-center text-md-left">Since its launch in 2002, the IvyDB database has been the industry standard for historical option prices and implied volatility data. Used by over 300 institutions, IvyDB contains accurate end-of-day prices for options along with their correctly calculated implied volatilities and greeks. With IvyDB, you’ll be able to evaluate risk models, test trading strategies, and perform sophisticated research on all aspects of the options markets.</p>
-                </div>
-                <div class="tab-pane fade show" id="nav-canada" role="tabpanel">
-                  <p class="section-text lato-light text-center text-md-left">Since its launch in 2002, the IvyDB database has been the industry standard for historical option prices and implied volatility data. Used by over 300 institutions, IvyDB contains accurate end-of-day prices for options along with their correctly calculated implied volatilities and greeks. With IvyDB, you’ll be able to evaluate risk models, test trading strategies, and perform sophisticated research on all aspects of the options markets.</p>
-                </div>
-                <div class="tab-pane fade show" id="nav-europe" role="tabpanel">
-                  <p class="section-text lato-light text-center text-md-left">Since its launch in 2002, the IvyDB database has been the industry standard for historical option prices and implied volatility data. Used by over 300 institutions, IvyDB contains accurate end-of-day prices for options along with their correctly calculated implied volatilities and greeks. With IvyDB, you’ll be able to evaluate risk models, test trading strategies, and perform sophisticated research on all aspects of the options markets.</p>
-                </div>
-                <div class="tab-pane fade show" id="nav-asia" role="tabpanel">
-                  <p class="section-text lato-light text-center text-md-left">Since its launch in 2002, the IvyDB database has been the industry standard for historical option prices and implied volatility data. Used by over 300 institutions, IvyDB contains accurate end-of-day prices for options along with their correctly calculated implied volatilities and greeks. With IvyDB, you’ll be able to evaluate risk models, test trading strategies, and perform sophisticated research on all aspects of the options markets.</p>
-                </div>
-                <div class="tab-pane fade show" id="nav-global" role="tabpanel">
-                  <p class="section-text lato-light text-center text-md-left">Since its launch in 2002, the IvyDB database has been the industry standard for historical option prices and implied volatility data. Used by over 300 institutions, IvyDB contains accurate end-of-day prices for options along with their correctly calculated implied volatilities and greeks. With IvyDB, you’ll be able to evaluate risk models, test trading strategies, and perform sophisticated research on all aspects of the options markets.</p>
-                </div>
-              </div>
-            </div>
-            <div class="tab-pane fade" id="nav-optigraph" role="tabpanel" aria-labelledby="nav-optigraph-tab">World</div>
-          </div>
+    <div class="position-relative page-wrapper-up py-3 p-lg-5">
+      <div class="row d-md-none">
+        <div class="col-12 text-center">
+          <p class="section-title text-blue montserrat-regular">Data Analysis Products</p>
         </div>
       </div>
+      <?php get_template_part( 'partials/maps' ); ?>
       <div class="row d-flex align-items-center mb-5 pt-5 mx-0">
         <div class="col-xs-12 col-sm-12 col-lg-6 col-md-6 col-xl-6 d-sm-none d-none d-lg-block d-xl-block d-md-block">
           <div class="tab-content" id="nav-tabContentGraph">
